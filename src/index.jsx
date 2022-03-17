57750c5c8c286e43d0923adb0fb1cac970296c35
vinnidias <vinicius.3991@gmail.com>
2022-03-17 20:15:42 -0300

    passando estilo global para o app

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './templates/App';
+import { GlobalStyles} from './styles/global-styles'
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Home />
+    <GlobalStyles/>
   </React.StrictMode>,
   document.getElementById('root')
 );
